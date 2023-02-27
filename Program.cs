0ed3609ffd8e26f3d6b480fff3abeae86aadf299
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 11:39:20 +0100

    Input Data updated

--- Program.cs
+++ Program.cs
@@ -7,9 +7,22 @@
  */
 
 //Input Data
+Console.WriteLine("Welcome to the Student Check Console Application");
+Console.WriteLine("_________________________________________________");
+Console.WriteLine();
+
+Console.Write("What is your FIRST NAME ?: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("How old are you?: ");
+string? ageString = Console.ReadLine();
+
+int age = int.Parse(ageString);
 
 //Switch case
 
+
 //If Age, else if
+
 
 //close application
